aebe442418b1a3a8c946d985ca26bc9b1f5b82cb
ankitbhagat-bit <ankit.bhagat@socialchaat.com>
2026-04-02 17:00:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,6 +156,8 @@ if uploaded_file:
         "Customer Mobile",
         "Shipping Address Line 1",
         "Shipping Address Line 2",
+        "City, State",
+        "Pincode",
         "Pymt",
         "word_count",
         "risk_score",
